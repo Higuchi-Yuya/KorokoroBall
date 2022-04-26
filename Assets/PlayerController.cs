ba463d200b47d9168747eeafccfeb0dc813718c2
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2022-04-26 15:40:31 +0900

    軽くステージ

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float power = 10;
+    public float power = 1;
     public Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
@@ -22,6 +22,14 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(new Vector3(1, 0, 0) * power);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, -1) * power);
         }
     }
 }
